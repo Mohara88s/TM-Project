8ff228e0921172f1fa3180fd273b2eaf166d40e9
Mohara88s <73835861+Mohara88s@users.noreply.github.com>
2022-06-09 11:35:22 +0300

    array of objects (adres time result)

--- bin/server.js
+++ bin/server.js
@@ -8,4 +8,20 @@ const { PORT = 3000 } = process.env
 app.listen(PORT)
 console.log(`Server running. Use our API on port: ${PORT}`)
 
-setInterval(modbusRtuLogger, 3000)
+
+const stationLog = async (max) => {
+  const array = []
+ 
+  for (let i = 1; i <= max; i += 1) {
+
+    const res = await modbusRtuLogger(i)
+    
+    // console.log(i, res)
+    let nowTime = new Date();
+    array.push({adres:i, time:nowTime, result:res})
+  }
+  console.log(array)
+}
+
+setInterval(stationLog, 2000, 2)
+
--- loggers/modbusRtuLogger.js
+++ loggers/modbusRtuLogger.js
@@ -1,35 +1,48 @@
 const modbus = require("jsmodbus");
 const SerialPort = require("serialport");
 
-const logger = () => {
+const logger = async (adres) => {
+	const promise = new Promise((resolve, reject) => {
+		const log = []
 	const socket = new SerialPort("COM4", {
 		baudRate: 9600,
 		parity: "none",
 		stopbits: 1,
 		dataBits: 8,
 	});
-	const adres = 1;
 
 	const client = new modbus.client.RTU(socket, adres);
 	// socket.on('close', function () {
 	//   console.log(arguments)
 	// })
 
-	socket.on("open", function () {
-		client
+	const clientLog = async () => {
+		const data = await client
 			.readHoldingRegisters(8196, 8)
 			.then(function (resp) {
-				let now = new Date();
-				console.log(now, "adres", adres, "values", resp.response._body._values);
+				
+				// console.log(now, "adres", adres, "values", resp.response._body._values);
 				socket.close();
+				return resp.response._body._values	
+				
 			})
 			.catch(function () {
-				console.error(arguments);
+				// console.error(arguments);
 				socket.close();
+				return []
 			});
-	});
+			log.push(...data)
+			resolve(log);
+	} 
 
-	socket.on("error", console.error);
+	socket.on("open", clientLog)
+
+	socket.on("error", () => {console.error; return 404});
+	  });
+	  
+	  let result = await promise;
+	//   console.log('result', result)
+	  return result
 };
 
 module.exports = logger;
